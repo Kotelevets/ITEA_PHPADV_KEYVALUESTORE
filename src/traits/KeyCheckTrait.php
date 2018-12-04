20e361085062a58004ee176ac6487a0066f714bc
Kotelevets <kotya.aa@gmail.com>
2018-12-04 14:42:12 +0200

    Added interface, trait and exception for key checking

---
+++ src/traits/KeyCheckTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\traits;
+use App\exceptions\KeyCheckException;
+
+class KeyCheckTrait
+{
+    /**
+     * @param $key
+     */
+    public function keyValidate($key)
+    {
+        if(is_string($key) || is_int($key)) {
+            return true;
+        }
+        throw new KeyCheckException(\sprintf("You can't use %s type for key", gettype($key)))
+    }
+}
